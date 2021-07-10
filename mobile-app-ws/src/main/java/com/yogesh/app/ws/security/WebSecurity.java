ab4021ea5ac8d209047c6b8255cc7fd4ee02ca1f
yogesh kumar solanki <yogeshsolankiglobal@gmail.com>
2021-07-10 15:26:49 +0530

    configure method delted

--- mobile-app-ws/src/main/java/com/yogesh/app/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/yogesh/app/ws/security/WebSecurity.java
@@ -25,10 +25,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
-	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
