3d2492c2856aa170ba90799ae1aea1e8cc44eba9
yogesh kumar solanki <yogeshsolankiglobal@gmail.com>
2021-07-16 15:23:35 +0530

    Remove Spaces

--- mobile-app-ws/src/main/java/com/yogesh/app/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/yogesh/app/ws/security/WebSecurity.java
@@ -10,45 +10,36 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.yogesh.app.ws.service.UserService;
 
 @EnableWebSecurity
-public class WebSecurity extends WebSecurityConfigurerAdapter{
-	
-	private final UserService userService;
-	
-	private final BCryptPasswordEncoder bCryptPasswordEncoder;
-	
-	
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-		
-		this.userService = userService;
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-	}
+    public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
-	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		
-		http.csrf().disable()
-		.authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
-		.permitAll()
-		.anyRequest().authenticated()
-		.and().addFilter(getAuthenticationFilter())
-		.addFilter(new AuthorizationFilter(authenticationManager()));
-		
-	}
-	
-	public AuthenticationFilter getAuthenticationFilter() throws Exception
-	{
-		final AuthenticationFilter filter=new AuthenticationFilter(authenticationManager());
-		filter.setFilterProcessesUrl("/users/login");
-		return filter;
-	}
-	
-	
-	
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
+                .permitAll()
+                .anyRequest().authenticated()
+                .and().addFilter(getAuthenticationFilter())
+                .addFilter(new AuthorizationFilter(authenticationManager()));
+
+    }
+
+    public AuthenticationFilter getAuthenticationFilter() throws Exception {
+        final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+        filter.setFilterProcessesUrl("/users/login");
+        return filter;
+    }
+
 
 }
